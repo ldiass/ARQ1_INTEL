09fbba1e5971831a4258f84dbceb1ba364c2ac1d
Luís Dias Ferreira Soares <ldiass@live.com>
2022-05-06 09:56:34 -0700

    Bug on sum solved. To be tested and refactorized

--- main.asm
+++ main.asm
@@ -40,8 +40,15 @@ col2			db	0
 col3			db	0
 col4			db	0
 flags_aux		db	0
-conta_byte		db	0
+conta_byte		dw	0
 buff_dl			db	0
+; Variável interna usada na rotina printf_w
+BufferWRWORD	db		10 dup (?)
+
+; Variaveis para uso interno na função sprintf_w
+sw_n	dw	0
+sw_f	db	0
+sw_m	dw	0
 
 
 MAXSTRING	equ		200
@@ -200,23 +207,15 @@ TerminouArquivo:
 	;Printa o contador de bytes na tela
 	lea		bx, MsgBytes
 	call		printf_s	
-	mov		dl, conta_byte
-	and		dl, UN	;Colocar o upper nibble em baixo
-	SHR		dl,1
-	SHR		dl,1
-	SHR		dl,1
-	SHR		dl,1
-	add		dl, 30h	;Se for um numero de 0-9, soma so 30
-	mov 		ah,2
-	int		21h
-	mov		dl, conta_byte ;Insere novamente td o valor em dl
-	and		dl, LN	;Limpa o upper nibble
-	add		dl, 30h	;Se for um numero de 0-9, soma so 30
-	mov 		ah,2
-	int		21h
+	mov		ax, conta_byte
+	call 		printf_w
+
+
 	lea		bx, MsgCRLF
 	call	printf_s
 	
+
+
 	;Printa o valor da soma
 	lea		bx, MsgSoma
 	call	printf_s
@@ -659,6 +658,77 @@ printf_s	proc	near
 ps_1:
 	ret
 printf_s	endp
+
+;
+;--------------------------------------------------------------------
+;Função: Escreve o valor de AX na tela
+;		printf("%
+;--------------------------------------------------------------------
+printf_w	proc	near
+	; sprintf_w(AX, BufferWRWORD)
+	lea		bx,BufferWRWORD
+	call	sprintf_w
+	
+	; printf_s(BufferWRWORD)
+	lea		bx,BufferWRWORD
+	call	printf_s
+	
+	ret
+printf_w	endp
+
+
+;
+;--------------------------------------------------------------------
+;Função: Converte um inteiro (n) para (string)
+;		 sprintf(string->BX, "%d", n->AX)
+;--------------------------------------------------------------------
+sprintf_w	proc	near
+	mov		sw_n,ax
+	mov		cx,5
+	mov		sw_m,10000
+	mov		sw_f,0
+	
+sw_do:
+	mov		dx,0
+	mov		ax,sw_n
+	div		sw_m
+	
+	cmp		al,0
+	jne		sw_store
+	cmp		sw_f,0
+	je		sw_continue
+sw_store:
+	add		al,'0'
+	mov		[bx],al
+	inc		bx
+	
+	mov		sw_f,1
+sw_continue:
+	
+	mov		sw_n,dx
+	
+	mov		dx,0
+	mov		ax,sw_m
+	mov		bp,10
+	div		bp
+	mov		sw_m,ax
+	
+	dec		cx
+	cmp		cx,0
+	jnz		sw_do
+
+	cmp		sw_f,0
+	jnz		sw_continua2
+	mov		[bx],'0'
+	inc		bx
+sw_continua2:
+
+	mov		byte ptr[bx],0
+	ret		
+sprintf_w	endp
+
+
+
 
 
 ;--------------------------------------------------------------------
